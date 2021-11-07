b3e83b4673ea306f734a4e92ccbf79c787b11533
sky03 <skyls03@qq.com>
2021-11-08 01:14:47 +0800

    优化各种细节

--- src/cn/sky03/structural/proxy/dynamicProxy/jdkDynamicProxy/Client2.java
+++ src/cn/sky03/structural/proxy/dynamicProxy/jdkDynamicProxy/Client2.java
@@ -7,15 +7,20 @@ import java.lang.reflect.Proxy;
 public class Client2 {
 
     public static void main(String[] args) {
+        // 实例化被代理对象
+        UserService userService = new UserServiceImpl();
+        // 拿到代理对象
         UserService service = (UserService) Proxy.newProxyInstance(
-                Thread.currentThread().getContextClassLoader(),
-                new Class[]{UserService.class},
-                new UserServiceInvocationHandler(new UserServiceImpl()));
+                userService.getClass().getClassLoader(),
+                userService.getClass().getInterfaces(),
+                new UserServiceInvocationHandler(userService));
+        // 由代理对象来执行方法
         service.add();
-
     }
+
     private static class UserServiceInvocationHandler implements InvocationHandler{
 
+        // 接收被代理对象
         private final UserService userService;
 
         public UserServiceInvocationHandler(UserService userService) {
@@ -25,8 +30,9 @@ public class Client2 {
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
             System.out.println("方法执行前。。。");
-            // returnVal是代理方法的返回值；有就有，没有就没有
+            // returnVal是被代理方法的返回值；有就有，没有就没有
             // args是代理方法的参数；有就有，没有就没有
+            // userService是被代理的已经实例化的对象，实例化的对象才能执行方法
             Object returnVal = method.invoke(userService, args);
             System.out.println("方法执行后。。。");
             return returnVal;
